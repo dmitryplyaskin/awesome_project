bc90ba29f196b23b1a050b4ea9bb67651eb5fff3
Dmitry <dima28rus97@gmail.com>
2019-08-07 22:00:02 +1000

    graphql

--- backend/src/bin/main.rs
+++ backend/src/bin/main.rs
@@ -1,17 +1,76 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Arc;
+
+#[macro_use]
+extern crate juniper;
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, Error};
+use futures::future::Future;
+use juniper::http::graphiql::graphiql_source;
+use juniper::http::GraphQLRequest;
+use serde::Deserialize;
+
+mod schema;
+use crate::schema::{create_schema, Schema};
 
 fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-fn index2(info: web::Path<(String)>) -> impl Responder {
-    let a = format!("Hello world! {}", info);
+#[derive(Deserialize)]
+struct Info {
+    color: String,
+    other: Option<String>,
+}
+
+fn index2((path, query): (web::Path<(String)>, web::Query<Info>)) -> impl Responder {
+    let a = format!(
+        "Hello world! path={} color={} other={}",
+        path,
+        query.color,
+        match query.other {
+            None => "No other query",
+            Some(ref x) => x,
+        }
+    );
     return HttpResponse::Ok().body(a);
 }
 
+fn graphiql() -> HttpResponse {
+    let html = graphiql_source("http://127.0.0.1:8080/graphql");
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(html)
+}
+
+fn graphql(
+    st: web::Data<Arc<Schema>>,
+    data: web::Json<GraphQLRequest>,
+) -> impl Future<Item = HttpResponse, Error = Error> {
+    web::block(move || {
+        let res = data.execute(&st, &());
+        Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
+    })
+    .map_err(Error::from)
+    .and_then(|user| {
+        Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(user))
+    })
+}
+
+
+
 fn main() {
-    HttpServer::new(|| {
+
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    // Create Juniper schema
+    let schema = std::sync::Arc::new(create_schema());
+
+    HttpServer::new(move || {
         App::new()
+            .data(schema.clone())
+            .service(web::resource("/graphql").route(web::post().to_async(graphql)))
+            .service(web::resource("/graphiql").route(web::get().to(graphiql)))
             .route("/", web::get().to(index))
             .route("/{name}", web::get().to(index2))
     })
---
+++ backend/src/bin/schema.rs
@@ -0,0 +1,44 @@
+use juniper::FieldResult;
+use juniper::RootNode;
+
+#[derive(GraphQLObject)]
+#[graphql(description = "Wow, it's work!")]
+struct User {
+  age: String,
+  name: String,
+}
+
+#[derive(GraphQLInputObject)]
+#[graphql(description = "New User")]
+struct NewUser {
+  age: String,
+  name: String,
+}
+
+pub struct QueryRoot;
+
+graphql_object!(QueryRoot: () |&self| {
+    field user(&executor) -> FieldResult<User> {
+        Ok(User{
+            age: "22".to_owned(),
+            name: "Dmitry".to_owned(),
+        })
+    }
+});
+
+pub struct MutationRoot;
+
+graphql_object!(MutationRoot: () |&self| {
+    field createHuman(&executor, new_user: NewUser) -> FieldResult<User> {
+        Ok(User{
+            age: new_user.age,
+            name: new_user.name,
+        })
+    }
+});
+
+pub type Schema = RootNode<'static, QueryRoot, MutationRoot>;
+
+pub fn create_schema() -> Schema {
+  Schema::new(QueryRoot {}, MutationRoot {})
+}
